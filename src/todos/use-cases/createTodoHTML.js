1b9b43405f0ab99d543f951eca104908c31e3332
Sofía J <sofiajimglez@gmail.com>
2023-06-05 14:02:11 +0200

    improvement: todo item inner HTML

--- src/todos/use-cases/createTodoHTML.js
+++ src/todos/use-cases/createTodoHTML.js
@@ -6,8 +6,20 @@ import { Todo } from '../models/todo.model';
  */
 export const createTodoHTML = (todo) => {
   if (!todo) throw new Error('A Todo object is required.')
-  const html = `<h3>${todo.description}</h3>`;
+
+  const { done, description, id } = todo;
+
+  const html = `<div class="view">
+      <input class="toggle" type="checkbox" ${done ? 'checked' : ''}>
+      <label>${description}</label>
+      <button class="destroy"></button>
+    </div>
+    <input class="edit" value="Create a TodoMVC template">`;
+  
   const liElement = document.createElement('li');
   liElement.innerHTML = html;
+  liElement.setAttribute('data-id', id);
+  if (done) liElement.classList.add('completed');
+
   return liElement;
 }
