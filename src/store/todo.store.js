1a879af3597212285741b3d8a257c6e16a1c8965
Sofía J <sofiajimglez@gmail.com>
2023-06-05 13:18:07 +0200

    feat: implements store methods

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -16,9 +16,78 @@ const state = {
 
 const initStore = () => {
   console.log(state);
-  console.log('init store 🥑');
+  console.log('Init store 🥑');
+};
+
+const loadStore = () => {
+  throw new Error('Not implemented');
+};
+
+const getTodos = (filter = Filters.All) => {
+  switch(filter) {
+    case Filters.All:
+      return [...state.todos];
+    case Filters.Completed:
+      return state.todos.filter(todo => todo.done);
+    case Filters.Pending:
+      return state.todos.filter(todo => !todo.done);
+    default:
+      throw new Error(`${filter} is not a valid filter.`);
+  }
+};
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+  if (!description) throw new Error('Description is required');
+
+  state.todos.push(new Todo(description));
+};
+
+
+const toggleTodo = (todoId) => {
+  state.todos = state.todos.map(todo => {
+    if (todo.id === todoId) {
+      todo.done = !todo.done;
+      return todo;
+    } else {
+      return todo;
+    }
+  })
+};
+
+const deleteTodo = (todoId) => {  
+  state.todos = state.todos.filter(todo => todo.id !== todoId);
+}
+
+const deleteCompleted = () => {
+  state.todos = state.todos.filter(todo => !todo.done);
+};
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = (newFilter = Filters.All) => {
+  if (!Object.keys(Filters).includes(newFilter)) throw new Error(`${newFilter} is not a valid filter.`);
+
+  state.filter = newFilter;
+};
+
+const getCurrentFilter = () => {
+  return state.filter.toString();
 };
 
 export default {
-  initStore
-}
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
+  getCurrentFilter,
+  getTodos,
+  initStore,
+  loadStore,
+  setFilter,
+  toggleTodo
+};
